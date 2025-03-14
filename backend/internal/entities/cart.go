f550ec2640f87edceafe3b7b8ee9a5cfa3fc1f29
pasit10 <pasit1006@gmail.com>
2025-03-15 00:22:47 +0700

    kvghjgo

--- backend/internal/entities/cart.go
+++ backend/internal/entities/cart.go
@@ -1,18 +1,18 @@
 package entities
 
 type InsertCart struct {
-	IDproduct int32 `json:"idproduct"`
+	IDproduct int32 `json:"id"`
 	Quantity  int32 `json:"qty"`
 }
 
 type Cart struct {
 	UID       string `json:"uid"`
-	IDproduct int32  `json:"idproduct"`
+	IDproduct int32  `json:"id"`
 	Quantity  int32  `json:"qty"`
 }
 
 type CartResponse struct {
-	IDproduct    string  `json:"idproduct"`
+	IDproduct    string  `json:"id"`
 	Productname  string  `json:"product_name"`
 	Priceperunit float64 `json:"price_per_unit"`
 	Quantity     int32   `json:"qty"`
